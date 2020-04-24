a5a97b58210ac070d0bbbee68ba08d7c66fa8efd
avossen <agvossen@gmail.com>
2020-04-24 16:17:57 -0400

    update to study

--- PlotDiHadKins.py
+++ PlotDiHadKins.py
@@ -78,7 +78,7 @@ def plotXVsEta(data):
     fig, ax=plt.subplots(1,1)
     #histogram returns histogram plus axes...
     z=ma.masked_where(hist2d[0] <= 0, hist2d[0])
-    cf=plt.contourf(etaMean, xMean, z, alpha=.75, cmap='jet',locator=ticker.LogLocator(numticks=20,subs=range(1,10)))
+    cf=plt.contourf(xMean, etaMean, z, alpha=.75, cmap='jet',locator=ticker.LogLocator(numticks=20,subs=range(1,10)))
   #  cf=plt.contourf(etaMean, np.log10(xMean), z, 8, alpha=.75, cmap='jet',locator=ticker.LogLocator())
     ax.set_yscale("log")
     ax.yaxis.set_major_locator(ticker.LogLocator(subs=range(1,10)))
@@ -88,6 +88,52 @@ def plotXVsEta(data):
     ax.set_title("$\eta$ vs $x$")
     plt.xlabel('$\eta$')
     plt.ylabel('$x$')
+    return
+#do regular 1d plot, takes some panda
+def plotXVsQ2(data):
+  
+    xEdges=np.logspace(-3,-1,30)
+
+    
+    #xEdgesHigh=xEdgesLow[1:]
+    q2Edges=np.logspace(-1,4,30)
+    #this stores the eta for each hadron in the pair, so we have to flatten this structure
+    arQ2=data["Q2"]
+#    arQ2=arEta.flatten();
+    #get x for the other dimension, now of course we have only have as many x values
+    arX=data["x"]
+#    arX=np.repeat(arX,2)
+    hist2d=np.histogram2d(arX,arQ2,bins=(xEdges,q2Edges))
+    #should be done better, namely at mean of log
+    xMean=[exp((log(xEdges[i])-log(xEdges[i-1]))/2+log(xEdges[i-1]))  for i in range(len(xEdges)) if(i>0)]
+    q2Mean=[exp((log(q2Edges[i])-log(q2Edges[i-1]))/2+log(q2Edges[i-1]))  for i in range(len(q2Edges)) if(i>0)]
+    #don't cut off half of the upper and lower bins
+    xMean[0]=xEdges[0]
+    xMean[-1]=xEdges[-1]
+    q2Mean[0]=q2Edges[0]
+  #  q2Mean[-1]=q2Edges[-1]
+
+  
+    
+    fig, ax=plt.subplots(1,1)
+    #histogram returns histogram plus axes...
+    z=ma.masked_where(hist2d[0] <= 0, hist2d[0])
+    lev_exp = np.arange(np.floor(np.log10(z.min())-1),
+                    np.ceil(np.log10(z.max())+1))
+    levs = np.power(10, lev_exp)
+    cf=plt.contourf(xMean, q2Mean, z, alpha=.75, cmap='jet',locator=ticker.LogLocator(numticks=20,subs=range(1,10)))
+  #  cf=plt.contourf(etaMean, np.log10(xMean), z, 8, alpha=.75, cmap='jet',locator=ticker.LogLocator())
+    ax.set_yscale("log")
+    ax.yaxis.set_major_locator(ticker.LogLocator(subs=range(1,10)))
+    ax.yaxis.set_minor_locator(ticker.LogLocator(numticks=30))
+    ax.set_xscale("log")
+    ax.xaxis.set_major_locator(ticker.LogLocator(subs=range(1,10)))
+    ax.xaxis.set_minor_locator(ticker.LogLocator(numticks=30))
+  #  C = plt.contour(etaMean,xMean, z, 8, colors='black', linewidth=.5)
+    fig.colorbar(cf)
+    ax.set_title("$Q^2$ vs $x$")
+    plt.xlabel('$x$')
+    plt.ylabel('$Q^2$')
     return
 
 #do regular 1d plot, takes some panda
@@ -128,8 +174,8 @@ def plotXVsEtaPt(data):
     arX=data["x"]
     arX=np.repeat(arX,2)
     
-    hist2d=np.histogram2d(arX,arPt,bins=(xEdges,ptEdges),weights=arX)
-    hist2d_unweighted=np.histogram2d(arX,arPt,bins=(xEdges,ptEdges))
+    hist2d=np.histogram2d(arX,arPt,bins=(etaEdges,ptEdges),weights=arX)
+    hist2d_unweighted=np.histogram2d(arX,arPt,bins=(etaEdges,ptEdges))
     
     z=ma.masked_where(hist2d[0] <= 0, hist2d[0])
     z_unweighted=ma.masked_where(hist2d_unweighted[0] <= 0, hist2d_unweighted[0])
@@ -137,8 +183,8 @@ def plotXVsEtaPt(data):
     etaMean=[(etaEdges[i]-etaEdges[i-1])/2+etaEdges[i-1]  for i in range(len(etaEdges)) if(i>0)]
     ptMean=[(etaEdges[i]-etaEdges[i-1])/2+etaEdges[i-1]  for i in range(len(etaEdges)) if(i>0)]
     #don't cut off half of the upper and lower bins
-    ptMean[0]=xEdges[0]
-    ptMean[-1]=xEdges[-1]
+    ptMean[0]=ptEdges[0]
+    ptMean[-1]=ptEdges[-1]
     etaMean[0]=etaEdges[0]
     etaMean[-1]=etaEdges[-1]
     
